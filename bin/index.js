d463b5aa80e0b10135b3e8396512680def52626d
codealisha99 <codealisha99@gmail.com>
2025-09-26 23:48:44 +0530

    iitial code

---
+++ bin/index.js
@@ -0,0 +1,14 @@
+import chalk from 'chalk';
+import inquirer from 'inquirer';
+import gradient from 'gradient-string';
+import chalkAnimation from 'chalk-animation';
+import figlet from 'figlet';
+import { createSpinner } from 'nanospinner';
+
+
+
+
+
+console.log(chalk.bgGreen('hi mom'));
+
+
