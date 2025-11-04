75ad9903b7d11d5e02f21aaa2e9cf887f07b5e71
codealisha99 <codealisha99@gmail.com>
2025-11-05 01:12:19 +0530

    typescript scripts

--- typescript/index.ts
+++
@@ -1,6 +0,0 @@
-let x : number ;
-
-x = "error";
-
-console.log(x);
-
---
+++ typescript/src/index.ts
@@ -0,0 +1,14 @@
+function greet(user : {
+    name : string,
+    age : number
+})
+
+
+ let user = {
+    name : "alisha",
+    age : 25}
+
+
+
+
+greet(user);
