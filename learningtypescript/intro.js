0c80d9b9cca925e77c5d745c5ca555ef153607c9
codealisha99 <codealisha99@gmail.com>
2026-01-10 01:49:37 +0530

    starting typescript

---
+++ learningtypescript/intro.js
@@ -0,0 +1,3 @@
+var user = { name: "Alice", age: 30 };
+console.log("alice");
+console.log(user.name);
---
+++ learningtypescript/variablesMe.ts
@@ -0,0 +1,12 @@
+let greetings: string = "Hello, TypeScript!";
+
+let mynum: number = 42;
+//greetings. // it shows all the string methods 
+console.log(greetings);
+
+let isActive: boolean = true;
+
+
+export {};//removes error temporarily
+
+
